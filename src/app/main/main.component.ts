f1e30ba293a869d83401a458c04f0738e1b4c62e
Funminiyi Adedoyin <doyinfummi2012@gmail.com>
2022-05-03 16:12:07 +0100

    change bg color added to section 2 on main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
+  checkTheme = false;
+  toggleTheme() {
+    this.checkTheme = !this.checkTheme
+  }
   constructor() { }
 
   ngOnInit(): void {
